0747c94d4e710e31a23761763a9cc6232de0afbf
Matt Margolin <matt.margolin@gmail.com>
2014-04-26 08:49:48 -0400

    Fix Options Parser to use @options.send

--- modules/options.rb
+++ modules/options.rb
@@ -13,6 +13,7 @@ class Muby_Options_Parser
 		@options.verbose = false
 		#@options = {}#OpenStruct.new
 		@@params = params
+		@version = "Muby Tools Version: 0.1"
 		self.start
 	end
 
@@ -23,40 +24,36 @@ class Muby_Options_Parser
 #puts String.color_matrix
 		OptionParser.new do |opts|
 			opts.banner = "Banner"#""Usage init.rb [options]"+"\n"+usage
-			opts.on("-f", "--force ","Forcefully Delete Backup") do |v|
-				@options[:force] = v
-			end
 
 			opts.separator ""
 			opts.separator "Common options:"
-			opts.on("-v", "--[no]verbose","Run verbosely") do |v|
-				@options[:verbose] = v
-			end
+			#custom parameter binding for commands
 			@@params.each { |custom_param|
 				opts.on("--#{custom_param['string']}=INPUT","#{custom_param['description']}") do |v|
 					string = custom_param['string']
 					@options.send("#{string}=", v)
 				end
-				next;
 			}
-			#opts.on("--directory=DIR","Print command only") do |v|
-			#	@options[:directory] = v
-			#end
+			opts.on("-v", "--[no]verbose","Run verbosely") do |v|
+				@options.send("verbose=", true)
+			end
+			opts.on("-f", "--force ","Forcefully Delete Backup") do |v|
+				@options.send("force=", true)
+			end
 			opts.on("-p", "--print","Print command only") do |v|
-				@options[:print_only] = true
+				@options.send("print_only=", true)
 			end
 			opts.on("-s", "--silent","Run silently") do |v|
-				@options[:silent] = v
+				@options.send("silent=", true)
 			end
-
 			opts.on_tail("-h", "--help", "Show this message") do
 				puts opts
 				exit
-			  end
+		  end
 
 			  # Another typical switch to print the version.
 			  opts.on_tail("--version", "Show version") do
-				puts OptionParser::Version.join('.')
+				puts @version
 				exit
 			  end
 
