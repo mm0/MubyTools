b753d949dc672122aace0c6a40996b49b84521ec
Matt Margolin <matt.margolin@gmail.com>
2014-04-26 08:47:35 -0400

    Update SSH commands

--- modules/commands/ssh.rb
+++ modules/commands/ssh.rb
@@ -10,6 +10,7 @@ include Command
 	def initialize
 		init
 		@sort 			=	0
+		@ENABLED		=	false
 		@category		=	"Local CLI Commands"
 		@sub_category 	=	"Commands to be executed locally through a Shell"
 		@command_type	=	Current_User
@@ -21,13 +22,14 @@ include Command
 	def installed? 
 		super
 		@@binary = `which ssh`
+		return @@binary
 	end
 end
 
 class SSH_connect < SSH
 	def initialize
 		super
-		@ENABLED		= true
+		init
 		@sort 			= 100
 		@command		=	"ssh {user}@{server}"
 		@title			=	"Connect to Remote Server"
@@ -51,13 +53,28 @@ end
 class SSH_remote_command < SSH
 	def initialize
 		super
-		@title 			= "Compress File(s)"
-		@ENABLED		= true
+		init
+		@title 			= "Send Command to Remote Server via SSH"
 		@description 	= "Send Command to Remote Server via SSH"
 		@shortcut 		= "ssh_execute_remote_cmd"
-		@command		=	"ssh $1 '$2'"
+		@command		=	"ssh {user}@{server} '{command}'"
 		@sort 			= 200
 		@category		= "Remote CLI Commands"
 		@sub_category 	= "Commands to be executed remotely through SSH"
 	end
+    def get_input_array
+        user_input       = TTY_input.new
+        user_input.required = true
+        user_input.name  =   "User"
+        user_input.string=   "user"
+        server_input       = TTY_input.new
+        server_input.required = true
+        server_input.name  =   "Server Address"
+        server_input.string=   "server"
+        command_input       = TTY_input.new
+        command_input.required = true
+        command_input.name  =   "Command"
+        command_input.string=   "command"
+        return [user_input,server_input,command_input]
+    end 
 end
